4024f9ad882400ccb89ee46b5edf842b31be6d01
Said Ahmad <saidahmad.batang@outlook.com>
2022-02-08 16:50:44 +0700

    Upgrading wablast

---
+++ wablast.py
@@ -0,0 +1,123 @@
+from selenium import webdriver
+import time
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import pandas as pd
+
+class Blast:
+    options = webdriver.ChromeOptions()
+    options.add_argument(r'--user-data-dir=C:\Users\Saidzzz\AppData\Local\Google\Chrome\User Data\Default')
+    options.add_argument('--profile-directory=Default')
+    s = Service('./chromedriver/chromedriver.exe')
+
+    def __init__(self):
+        self.driver = webdriver.Chrome(service=Blast.s, options=Blast.options)
+        self.wait = WebDriverWait(self.driver, 45)
+        self.fwd_number = 1
+        self.pause = 0.5
+        self.xpath = {
+            'messages': '//div[@class="Nm1g1 _22AX6"]',
+            'msg_option': '//span[@data-testid="down-context"]',
+            'forward': '//div[@aria-label="Forward message"]',
+            'fwd_btn': '//button[@title="Forward message"]',
+            'fwds_btn': '//button[@title="Forward messages"]',
+            'search_fwd': '//div[@role="textbox"]',
+            'send': '//div[@role="button"]',
+            'checkbox_fwd': '//div[@data-testid="visual-checkbox"]'
+        }
+
+    # Common commands
+    def klik(self, xpath: str):
+        self.clickable(xpath)
+        self.driver.find_element(By.XPATH, xpath).click()
+
+    def cek_n_klik(self, xpath, t):
+        self.doesexist(xpath, t)
+        self.driver.find_element(By.XPATH, xpath).click()
+
+    def access(self, url='https://web.whatsapp.com/'):
+        self.driver.get(url)
+
+    def choose_name_or_group(self, name='CHECK'):
+        self.klik(f'//span[@title="{name}"]')
+
+    def click_msg_option(self):
+        elems = self.driver.find_elements(By.XPATH, self.xpath['messages'])
+        ActionChains(self.driver).move_to_element(elems[-1]).perform()  # hover over
+        self.klik(self.xpath['msg_option'])
+
+    def click_fwd_msg(self):
+        self.klik(self.xpath['forward'])
+
+    def click_msg_to_fwd(self, n=1):
+        checkboxes = self.driver.find_elements(By.XPATH, self.xpath['checkbox_fwd'])
+        self.fwd_number = n
+        if n > 1:
+            for i in range(1, n):
+                checkboxes[-1 - i].click()
+        else:
+            pass
+
+    def click_fwd_btn(self):
+        if self.fwd_number == 1:
+            self.klik(self.xpath['fwd_btn'])
+        else:
+            self.klik(self.xpath['fwds_btn'])
+
+    def fill_click_target_fwd(self, name='Father'):
+        search = self.driver.find_element(By.XPATH, self.xpath['search_fwd'])
+        search.send_keys(name)
+        time.sleep(self.pause)
+        try:
+            self.cek_n_klik(f'//span[@title="{name}"]', t=3)
+        except ElementNotInteractableException:
+            print(f"Can't click {name}!")
+        except TimeoutException:
+            print(f"Can't find {name}!")
+        finally:
+            search.clear()
+
+    def send_fwd(self):
+        self.klik(self.xpath['send'])
+
+    def forward(self, source, n, targets: list):
+        self.choose_name_or_group(source)
+        time.sleep(self.pause)
+        self.click_msg_option()
+        time.sleep(self.pause)
+        self.klik(self.xpath['forward'])
+        time.sleep(self.pause)
+        self.click_msg_to_fwd(n)
+        time.sleep(self.pause)
+        self.click_fwd_btn()
+        time.sleep(self.pause)
+        for target in targets:
+            self.fill_click_target_fwd(target)
+        self.klik(self.xpath['send'])
+
+    # Other commands
+    def clickable(self, xpath):
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        time.sleep(self.pause)
+
+    def doesexist(self, xpath, t=45):
+        wait = WebDriverWait(self.driver, t)
+        wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+        time.sleep(self.pause)
+
+    def close(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    blast = Blast()
+    print("Yuhu")
+    time.sleep(10)
+    blast.access()
